d3857fa02226dcb4c6588e335da4ad66146569de
mark <mark.munene@student.moringaschool.com>
2022-08-31 16:14:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+
+const headquarters = 42;
+function distanceFromHqInBlocks(blocks){
+    if (blocks > 42){
+        return blocks - 42;
+    }
+    else{
+        return 42 - blocks;
+    }
+}
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks)*264;
+}
+function distanceTravelledInFeet(blocks,blocks1) {
+    const distanceTravelledInFeet = (distanceFromHqInFeet(blocks) - distanceFromHqInFeet(blocks1))
+    if ( distanceTravelledInFeet >= 0) {
+        return distanceTravelledInFeet;
+    } else {
+        return ( distanceTravelledInFeet * -1);
+    }
+  }
+  function calculatesFarePrice(start,destination){
+    const totalFeetTravelled= (distanceTravelledInFeet(start, destination))
+    if (totalFeetTravelled <=400) {
+        return 0;
+    }else if (totalFeetTravelled >=400 && totalFeetTravelled <= 2000) {
+        return ((totalFeetTravelled - 400) * 0.02);
+    }else if (totalFeetTravelled >= 2000 && totalFeetTravelled <= 2500) {
+        return 25;
+    } else if (totalFeetTravelled >2500){
+        return 'cannot travel that far';
+    }
+  }
